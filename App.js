99a96ea6ab5d5f9b166accb34bdabf4824d39b0f
amuppalla4 <79055695+amuppalla4@users.noreply.github.com>
2021-09-02 13:49:00 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Feed from './screens/Feed';
+import CreateStory from './screens/CreateStory';
+import Color from 'color';
+
+const Tab=createBottomTabNavigator()
+
+export default function App() {
+  return (
+      <NavigationContainer>
+        <Tab.Navigator 
+        screenOptions={({route})=>({
+          tabBarIcon:({focused, color, size})=>{
+            let iconName;
+            if (route.name==='Feed'){
+              iconName=focused?
+              'book'
+              :'book-outline'
+            }else if (route.name==='CreateStory'){
+              iconName=focused?
+              'create'
+              :'create-outline'
+            }
+            return <Ionicons name={iconName} size={size} color={color} />
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor:'tomato',
+          inactiveTintColor:'gray',
+        }}>
+          <Tab.Screen name="Feed" component={Feed} />
+          <Tab.Screen name="CreateStory" component={CreateStory} />
+          
+        </Tab.Navigator>
+      </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
